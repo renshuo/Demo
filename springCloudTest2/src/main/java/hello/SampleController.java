b813f14831eaa5ceb095b43e1fa0f815dd544474
sren <renshs@gmail.com>
2016-11-13 01:17:12 +0000

    修复编译错误

--- springCloudTest2/src/main/java/hello/SampleController.java
+++ springCloudTest2/src/main/java/hello/SampleController.java
@@ -6,6 +6,7 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.*;
