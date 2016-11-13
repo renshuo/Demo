66c65ea6e238bca54bda3570826bb4137fd2cdbb
sren <renshs@gmail.com>
2016-11-13 00:43:59 +0000

    增加springCloud测试工程

---
+++ springCloudTest2/src/main/java/hello/SampleController.java
@@ -0,0 +1,22 @@
+package hello;
+
+
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.*;
+
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SampleController {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+} 
