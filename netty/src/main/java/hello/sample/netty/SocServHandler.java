dcd41f08a906709914f57042eaf4ac602fd873ff
renshuo <sren@yg.com>
2017-01-16 13:43:20 +0000

    use file Channel

--- netty/src/main/java/hello/sample/netty/SocServHandler.java
+++ netty/src/main/java/hello/sample/netty/SocServHandler.java
@@ -1,15 +1,14 @@
 package hello.sample.netty;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufInputStream;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.ReferenceCountUtil;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.util.Arrays;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.Date;
 
 /**
@@ -19,15 +18,22 @@ public class SocServHandler extends ChannelInboundHandlerAdapter {
 
 
     public static final int BUFFER_SIZE = 1048576;
-    private FileOutputStream fos;
     static long sumx = 0;
     static long sumc = 0;
+
+    static long start;
+    static long end;
+
+    private FileOutputStream fos;
+    private FileChannel fc;
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         System.out.println("#### connect active....");
         File f = new File("/home/work/test."+System.currentTimeMillis());
         fos = new FileOutputStream(f);
+        fc = fos.getChannel();
+        start = System.currentTimeMillis();
         ctx.write(new Date());
         ctx.flush();
     }
@@ -35,19 +41,19 @@ public class SocServHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf in = (ByteBuf) msg;
-        int i = 0;
-        byte[] buf = new byte[BUFFER_SIZE];
         try {
-            while (in.isReadable()) { // (1)
-                i = in.readableBytes();
-                if (i>BUFFER_SIZE){
-                    i = BUFFER_SIZE;
-                }
-                in.readBytes(buf, 0, i);
-                sumx += i;
-                sumc ++;
-                fos.write(buf, 0, i);
-            }
+            ByteBuffer src = in.nioBuffer();
+            fc.write(src);
+            sumx = fc.position();
+            sumc ++;
+
+//            int i ;
+//            while( (i = in.readableBytes())>0) {
+//                in.readBytes(fos, i);
+//                sumx += i;
+//                sumc++;
+//            }
+            end = System.currentTimeMillis();
         } finally {
             ReferenceCountUtil.release(msg); // (2)
         }
@@ -58,7 +64,8 @@ public class SocServHandler extends ChannelInboundHandlerAdapter {
         System.out.println("#### channel inactive -----");
         fos.flush();
         fos.close();
-        System.out.println(""+(sumx)+": "+(sumx/sumc));
+        fc.close();
+        System.out.println(""+(sumx)+":" + (sumx/sumc) +": "+(sumx/(end-start)));
         ctx.write(System.currentTimeMillis());
         ctx.flush();
     }
